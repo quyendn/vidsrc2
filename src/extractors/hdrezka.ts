e83c33ce74cfc9ee125ec1b7e16c06efd0fd4011
quyendn <quyendn84@gmail.com>
2024-10-08 09:40:25 +0700

    quyendn

--- src/extractors/hdrezka.ts
+++ src/extractors/hdrezka.ts
@@ -144,70 +144,8 @@ class Hdrezka extends Provider {
         const { url: streamUrl, subtitle: streamSubtitle }  = await getStreamData(searchResult.id, translatorId, 'movie');
         const parsedVideos = parseVideoLinks(streamUrl);
         const parsedSubtitles = parseSubtitleLinks(streamSubtitle);
-        return parsedVideos;
-        const mediaurl =
-          this.baseUrl +
-          "v2/embed/" +
-          (isMovie ? `movie/${id}` : `tv/${id}/${season}/${episode}`);
-        console.log(mediaurl);
-  
-        const [title, movieId, v] = await (
-          await fetch(mediaurl, {
-            headers: {
-              ...this.headers,
-              Accept:
-                "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-              Connection: "keep-alive",
-              "Sec-Fetch-Dest": "document",
-            },
-          })
-        )
-          .text()
-          .then((resp) => {
-            console.log(resp.match(/<title>(.*?)<\/title>/)![1]);
-            return [
-              resp.match(/<title>(.*?)<\/title>/)![1],
-              resp.match(/data-id="(.*?)"/g)![1].split('"')[1],
-              resp.match(/var\s*v\s*=\s*"(.*?)"/)![1],
-            ];
-          });
-        const params: URLSearchParams = new URLSearchParams({
-          id: String(id),
-          type: isMovie ? "movie" : "tv",
-          v: String(v),
-          vrf: "uwu",
-          isMobile: "false", // Convert boolean to string
-          ...(isMovie
-            ? {}
-            : { season: String(season), episode: String(episode) }), // Ensure season and episode are strings if they're defined
-        });
-        console.log(this.baseUrl + `api/episodes/${id}/servers?${params}`);
-        var { data } = await (
-          await fetch(this.baseUrl + `api/episodes/${id}/servers?${params}`, {
-            headers: this.headers,
-          })
-        ).json();
-  
-        let hash = data.find(
-          (source: { name: string }) => source.name === "UpCloud"
-        ).hash;
-  
-        console.log(this.baseUrl + "api/source/" + hash, {
-          ...this.headers,
-          Referer: `${this.baseUrl}upcloud/e/${hash}?init=true&key=${v}`,
-        });
-  
-        const source = await (
-          await fetch(this.baseUrl + "api/source/" + hash, {
-            headers: {
-              ...this.headers,
-              Referer: `${this.baseUrl}upcloud/e/${hash}?init=true&key=${v}`,
-            },
-          })
-        ).json();
-        console.log(source);
         // return title;
-        return { title, ...source };
+        return { parsedSubtitles, ...parsedVideos };
       } catch (error) {
         console.log("faild ", error);
         throw error;
